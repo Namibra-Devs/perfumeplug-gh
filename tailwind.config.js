9ed88f5302f66867ba65b66c67b7cf383e13c5de
Tyler Gobka Bright <tgbright88@gmail.com>
2025-09-23 11:41:25 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+        },
+        secondary: {
+          500: '#6b7280',
+          600: '#4b5563',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
