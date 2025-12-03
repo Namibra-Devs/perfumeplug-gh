0af789551f4aa8a0a5815206206ba19ef5b621be
Tyler Gobka Bright <tgbright88@gmail.com>
2025-12-03 09:30:23 +0000

    fix: missing onNext on button

--- src/components/checkout/DeliveryDetailsForm.tsx
+++ src/components/checkout/DeliveryDetailsForm.tsx
@@ -113,7 +113,7 @@ const DeliveryDetailsForm: FC<Props> = ({
           Back
         </button>
 
-        <button className="bg-blue-600 text-white px-6 py-2.5 rounded-lg text-sm font-semibold hover:bg-blue-700">
+        <button onClick={(onNext)} className="bg-blue-600 text-white px-6 py-2.5 rounded-lg text-sm font-semibold hover:bg-blue-700">
           Continue to Payment
         </button>
       </div>
